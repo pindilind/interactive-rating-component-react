f643907522e51554640666856988e217e67d49c3
pindilind <linda.gustafsson@outlook.com>
2024-01-15 19:00:25 +0100

    lagt till design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
+
+    <div>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      SKRIV HÄR
+      
+      Feel free to remove these styles or customise in your own stylesheet 👍 
+ 
+    .attribution fontSize: 11px; text-align: center; 
+    .attribution a  color: hsl(228, 45%, 44%); 
+  
+   Rating state start 
+
+  How did we do?
+
+  Please let us know how we did with your support request. All feedback is appreciated 
+  to help us improve our offering!
+
+  1 2 3 4 5
+
+  Submit
+
+  Rating state end 
+
+Thank you state start
+
+  You selectedAdd rating here out of 5
+
+  Thank you!
+
+  We appreciate you taking the time to give a rating. If you ever need more support, 
+  don’t hesitate to get in touch!
+
+   Thank you state end -
+
+  
+  
+    
+  
+
+    </div>
     </div>
   );
 }
